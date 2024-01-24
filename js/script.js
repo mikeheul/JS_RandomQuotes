c0be0d03777ee1b099332f3799b84b35e79ad447
mikeheul <mickael.murmann@gmail.com>
2024-01-24 10:54:20 +0100

    refactoring + comments

--- js/script.js
+++ js/script.js
@@ -1,122 +1,126 @@
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', getQuotes)
-let favoriteQuotes = JSON.parse(localStorage.getItem('favoriteQuotes')) || [];
-
+// Selecting elements
+const btn = document.querySelector('.btn');
 const openModalLink = document.getElementById('openModalLink');
 const favoriteModal = document.getElementById('favoriteModal');
 const closeBtn = document.querySelector('.close');
 const favoriteQuotesContainer = document.getElementById('favoriteQuotesContainer');
+const quoteContainer = document.getElementById('quote-container');
 
-openModalLink.addEventListener('click', () => {
-    // Afficher les citations favorites
-    displayFavoriteQuotes();
-    // Ouvrir la modale
-    favoriteModal.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-    // Fermer la modale
-    favoriteModal.style.display = 'none';
-});
-
+// Event listeners
+btn.addEventListener('click', getQuotes);
+openModalLink.addEventListener('click', openFavoriteModal);
+closeBtn.addEventListener('click', closeFavoriteModal);
 window.addEventListener('click', (event) => {
-    // Fermer la modale si l'utilisateur clique en dehors de celle-ci
+    // Close the modal if the user clicks outside of it
     if (event.target === favoriteModal) {
-        favoriteModal.style.display = 'none';
+        closeFavoriteModal();
     }
 });
 
+// Initializations
+let favoriteQuotes = JSON.parse(localStorage.getItem('favoriteQuotes')) || [];
+
+// Display favorite quotes in the modal
+function openFavoriteModal() {
+    displayFavoriteQuotes();
+    favoriteModal.style.display = 'block';
+}
+
+// Close the favorite quotes modal
+function closeFavoriteModal() {
+    favoriteModal.style.display = 'none';
+}
+
+// Display favorite quotes in the modal
 function displayFavoriteQuotes() {
-    // Effacer le contenu actuel de la modale
     favoriteQuotesContainer.innerHTML = '';
 
-    // Récupérer les identifiants des citations favorites depuis le localstorage
+    // Retrieve favorite quote IDs from local storage
     let favoriteQuotesIds = JSON.parse(localStorage.getItem('favoriteQuotes')) || [];
 
     if (favoriteQuotesIds.length === 0) {
-        // Aucune citation favorite trouvée, afficher "No quotes"
+        // Display a message when there are no favorite quotes
         let noQuotesDiv = document.createElement('div');
         noQuotesDiv.textContent = 'No quotes';
         favoriteQuotesContainer.appendChild(noQuotesDiv);
     } else {
-    // Afficher les citations favorites
+        // Display each favorite quote
         favoriteQuotesIds.forEach(quoteId => {
             const quote = quotes.find(q => q.id === quoteId);
             if (quote) {
-                let quoteDiv = document.createElement('div');
-                quoteDiv.classList.add('quote-modal');
-
-                quoteDiv.innerHTML =
-                    `<div class=''>
-                        <p class='title-quote-modal'>${quote.title}</p>
-                        <p class=''>“${quote.content}”</p>
-                    </div> 
-                    <div class=''>
-                        <p>- ${quote.author}</p>
-                    </div>`;
+                // Create and append the HTML for each favorite quote
+                let quoteDiv = createQuoteElement(quote);
                 favoriteQuotesContainer.appendChild(quoteDiv);
             } 
         });
     }
 }
 
-function getQuotes() {
-    //let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    // console.table(randomQuote);
-    //displayQuote(randomQuote);
-    const quoteContainer = document.getElementById('quote-container')
+// Generate HTML for a quote element
+function createQuoteElement(quote) {
+    let quoteDiv = document.createElement('div');
+    quoteDiv.classList.add('quote-modal');
 
-    // Mélanger le tableau de citations
-    const shuffledQuotes = quotes.slice().sort(() => Math.random() - 0.5);
-
-    // Sélectionner 5 citations parmi les citations choisies aléatoirement
-    const selectedQuotes = shuffledQuotes.slice(0, 5);
-    
-    quoteContainer.innerHTML = '';
-    selectedQuotes.forEach(quote => {
-        let quoteDiv = document.createElement('div');
-        quoteDiv.classList.add('quote');
-        
-        quoteDiv.innerHTML = 
-        `<div class='quote-text'>
-            <p class='title'>${quote.title}</p>
-            <p class='content'>“${quote.content}”</p>
+    // Populate the quote element with HTML content
+    quoteDiv.innerHTML =
+        `<div class=''>
+            <p class='title-quote-modal'>${quote.title}</p>
+            <p class=''>“${quote.content}”</p>
         </div> 
-        <div class='quote-author'>
+        <div class=''>
             <p>- ${quote.author}</p>
-        </div>
-        <div class='heart' data-id='${quote.id}'>
-            <i class='${favoriteQuotes.includes(quote.id) ? "fa-solid active" : "fa-regular"} fa-heart' title='Add favorite'></i>
-        </div>
-        `;
-        quoteContainer.appendChild(quoteDiv);
+        </div>`;
 
-        // Sélectionner l'icône "coeur" et ajouter un écouteur d'événement au clic
-        const heartIcon = quoteDiv.querySelector('.heart');
-        heartIcon.addEventListener('click', () => toggleFavorite(quote.id));
-    })
+    return quoteDiv;
 }
 
+// Fetch and display quotes
+function getQuotes() {
+    // Shuffle the quotes array
+    const shuffledQuotes = quotes.slice().sort(() => Math.random() - 0.5);
+    // Select the first 5 shuffled quotes
+    const selectedQuotes = shuffledQuotes.slice(0, 5);
+    
+    // Clear the quote container
+    quoteContainer.innerHTML = '';
+    // Display each selected quote
+    selectedQuotes.forEach(quote => {
+        let quoteDiv = createQuoteElement(quote);
+        quoteDiv.classList.add('quote');
+
+        // Create and append the heart icon with event listener
+        const heartIcon = document.createElement('div');
+        heartIcon.classList.add('heart');
+        heartIcon.setAttribute('data-id', quote.id);
+        heartIcon.innerHTML = `<i class='${favoriteQuotes.includes(quote.id) ? "fa-solid active" : "fa-regular"} fa-heart' title='Add favorite'></i>`;
+
+        quoteDiv.appendChild(heartIcon);
+        quoteContainer.appendChild(quoteDiv);
+
+        // Add event listener to the heart icon for toggling favorite status
+        heartIcon.addEventListener('click', () => toggleFavorite(quote.id));
+    });
+}
+
+// Toggle favorite status and update localStorage
 function toggleFavorite(quoteId) {
     const heartIcon = document.querySelector(`.heart[data-id='${quoteId}'] i`);
 
-    // Vérifier si la citation est déjà dans les favoris
     if (favoriteQuotes.includes(quoteId)) {
-        // Retirer la citation des favoris
+        // Remove the quote from favorites
         favoriteQuotes = favoriteQuotes.filter(id => id !== quoteId);
-        heartIcon.classList.remove('fa-solid');
+        heartIcon.classList.remove('fa-solid', 'active');
         heartIcon.classList.add('fa-regular');
-        heartIcon.classList.remove('active');
     } else {
-        // Ajouter la citation aux favoris
+        // Add the quote to favorites
         favoriteQuotes.push(quoteId);
-        heartIcon.classList.add('fa-solid');
+        heartIcon.classList.add('fa-solid', 'active');
         heartIcon.classList.remove('fa-regular');
-        heartIcon.classList.add('active');
     }
 
-    // Mettre à jour le localstorage
+    // Update localStorage with the latest favorite quotes
     localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotes));
 }
 
+// Initial fetch and display of quotes
 getQuotes();
